700fcb28c00b6929925c73d53676e968f0c4357d
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-08-09 11:13:53 +0530

    done

--- blog/vlog/urls.py
+++ blog/vlog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import post_list,post_detail
 
 urlpatterns =[
+    path("post/<int:pk>",post_detail,name="post_detail"),
+
     path("", post_list , name="home"),
-    path("post/<int:pk",post_detail,name="post_detail"),
 ]
