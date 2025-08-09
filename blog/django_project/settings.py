5476545eb025c8f73733444d510fc3ae4424716c
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-08-10 04:36:39 +0530

    done

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "vlog",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL="home"
+LOGOUT_REDIRECT_URL="home"
