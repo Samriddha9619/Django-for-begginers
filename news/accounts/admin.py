ca8e72d6bf708948b6ce5676460ae5f1dd5cfa60
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-08-10 23:56:09 +0530

    10 done

--- news/accounts/admin.py
+++ news/accounts/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-# Register your models here.
 
-from .forms import Cucf,Cuchangef
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form=Cucf
-    form=Cuchangef
-    model=CustomUser
-    list_display=[
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
         "email",
         "username",
         "age",
         "is_staff",
     ]
-    fieldsets= UserAdmin.fieldsets + ((None,{"fields":("age",)}),)
-    add_fieldsets= UserAdmin.fieldsets + ((None,{"fields": ("age",)}),)
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
 
-admin.site.register(CustomUser,CustomUserAdmin) 
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -1,12 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm , UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-class Cucf(UserCreationForm):
-    class Meta:
-        model= CustomUser
-        fields= UserCreationForm.Meta.fields + ("age",)
 
-class Cuchangef(UserChangeForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields= UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
