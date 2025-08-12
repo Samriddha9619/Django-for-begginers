5696b428b53c7e10906c102255d80c588b70a486
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-08-12 19:43:50 +0530

    deployment

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -1,6 +1,8 @@
-
 from pathlib import Path
+from environs import Env
 
+env=Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,12 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e8x8wlkg-9_ziuu#6j+bdajxzx%&a=&hm!z5v3or9f#4upo(=z"
-
+SECRET_KEY= env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env.bool("DEBUG",default=False)
+ALLOWED_HOSTS = [".railway.com","localhost","127.0.0.1"]
 
 
 # Application definition
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "accounts",
     "pages",
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -67,10 +69,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL")
 }
 
 
@@ -109,7 +108,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS= [BASE_DIR/"static"]
+STATIC_ROOT= BASE_DIR/ "staticfiles"
+STORAGES={
+    "default":{
+        "Backend": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles":{
+        "BACKEND":"whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -121,3 +129,4 @@ LOGOUT_REDIRECT_URL= "home"
 CRISPY_ALLOWED_TEMPLATE_PACKS="bootstrap5"
 CRISPY_TEMPLATE_PACK="bootstrap5"
 EMAIL_BACKEND ="django.core.mail.backends.console.EmailBackend"
+CSRF_TRUSTED_ORIGINS=["https://+.railway.com"]
